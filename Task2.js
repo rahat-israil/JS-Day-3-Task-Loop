7b1034b755bcd36d8caf9c55d5f7150868ce327c
rahat-israil <rahat.bin.israil>
2024-09-02 17:54:02 +0600

    Complete Task 2

---
+++ Task2.js
@@ -0,0 +1,35 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+let i = 61;
+
+for (i = 61; i <= 100; i++) {
+  if (i % 2 === 1) {
+    console.log('All ODD numbers from 61 to 100 : ' + i);
+  }
+}
+
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+
+let num = 78;
+
+for (num = 78; num <= 98; num++) {
+  if (num % 2 === 0) {
+    console.log('All EVEN numbers from 78 to 98 : ' + num);
+  }
+}
